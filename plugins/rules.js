1bbd984c1be3db2f0d6310f3474054beb1e6408e
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-06-03 01:12:41 +0700

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -1,1 +1,23 @@
 
+let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
+╭─「 R U L E S 」
+│ DILARANG SARA.
+│ DILARANG MENGHINA BOT.
+│ BOT HANYA 1 DALAM 1 GROUP.
+│ JIKA KAMI MENDETEKSI TERDAPAT
+│ BOT LAIN DALAM GROUP 
+│ BOT AKAN KELUAR DARI GROUP
+│ BOT TIDAK BERTANGGUNG JAWAB
+│ ATAS PERINTAH YANG ANDA BERIKAN
+│ BOT TIDAK MENYIMPAN DATA ATAU MEDIA
+│ YANG ANDA BERIKAN PADA BOT INI.
+│ BOT 1000% MENJAGA PRIVASI USER.
+╰────
+
+Note:Jika Melanggar Akan Di Banned, tidak ada refund uang sewa.
+`.trim(), wm, 'MENU BOT', '.menu', global.ftroli) // Tambah sendiri kalo mau
+handler.help = ['rules']
+handler.tags = ['info']
+handler.command = /^rules$/i
+
+module.exports = handler
