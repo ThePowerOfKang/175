ed9fa379c5f10f19c8217159152b910871634aef
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-06-05 21:34:24 +0700

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,7 +1,7 @@
 let { execSync } = require('child_process')
 let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/fokusdotid/family-md.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    let stdout = execSync('git remote set-url origin https://github.com/hdiiofficial/175.git && git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner)
       require('fs')
         .readdirSync('plugins')
