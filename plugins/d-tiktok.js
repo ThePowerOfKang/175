8762ac4968740ddee9fbb384958ebf70edd9a9e9
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-06-06 21:43:03 +0700

    Update d-tiktok.js

--- plugins/d-tiktok.js
+++ plugins/d-tiktok.js
@@ -1,27 +1,57 @@
-const { tiktokdl } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-  const {
-    author: { nickname },
-    video,
-    description,
-  } = await tiktokdl(args[0])
-  const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
-  if (!url) throw "Can't download video!"
-  conn.sendFile(
-    m.chat,
-    url,
-    'tiktok.mp4',
-    `
-*Nickname:* ${nickname}
-*Description:* ${description}
-`.trim(),
-    m
-  )
-}
-handler.help = ['tiktok'].map((v) => v + ' <url>')
-handler.tags = ['downloader']
-handler.limit = true
-handler.command = /^(tik(tok)?(dl)?)$/i
 
+let handler = async(m,{text, conn}) => {
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+
+const clean = (data) => {
+	let regex = /(<([^>]+)>)/ig
+	data = data.replace(/(<br?\s?\/>)/ig, ' \n')
+	return data.replace(regex, '')
+}
+
+async function tiktok(query) {
+   return new Promise((resolve, reject) => {
+     axios("https://lovetik.com/api/ajax/search", {
+       method: "POST",
+       data: new URLSearchParams(Object.entries({ query }))
+     })
+     .then(({ data }) => {
+     if (!data.vid) {
+        resolve(data)
+        return !0
+     }
+        resolve({
+        desc: clean(data.desc),
+        uploader: clean(data.author),
+        thumbnail: data.cover,
+        medias: {
+          nowm: {
+             size: data.links[0].s || '',
+             url: data.links[0].a || ''
+                  },
+          watermark: {
+             size: data.links[1].s || '',
+             url: data.links[1].a || ''
+                       },
+          audio: {
+             sound: clean(data.links[2].s || ''),
+             url: data.links[2].a || ''
+                  }
+                 }
+        })
+      })
+   })
+}
+
+result = await tiktok(text)
+nowm = result.medias.nowm.url;
+conn.sendFile(m.chat, nowm, null, 'Nih Bang', global.ftroli)
+}
+handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tiktok)$/i
+handler.register = true
+handler.limit = true
 module.exports = handler
