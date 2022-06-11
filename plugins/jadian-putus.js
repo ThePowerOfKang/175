61fd4bce0002abe0431882c5ce62fb5bdeef2de2
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-06-12 03:30:50 +0700

    Update jadian-putus.js

--- plugins/jadian-putus.js
+++ plugins/jadian-putus.js
@@ -36,7 +36,7 @@ let handler = async (m, { conn }) => {
   }
 }
 handler.help = ['putus']
-handler.tags = ['jadian']
+handler.tags = ['fun']
 handler.command = /^(putus)$/i
 handler.group = true
 handler.fail = null
