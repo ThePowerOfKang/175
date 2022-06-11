8ed43cb98c9fd05ea9442bd6b07bc244cca6c5a6
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-06-12 03:31:19 +0700

    Update jadian-terima.js

--- plugins/jadian-terima.js
+++ plugins/jadian-terima.js
@@ -50,7 +50,7 @@ let handler = async (m, { conn, text }) => {
   }
 }
 handler.help = ['terima *@tag*']
-handler.tags = ['jadian']
+handler.tags = ['fun']
 handler.command = /^(terima)$/i
 handler.group = true
 handler.limit = false
