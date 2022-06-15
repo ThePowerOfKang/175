ce3a838d00865016d2bdb1dad7f1ba00c8b4ce0a
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-06-15 18:04:49 +0000

    Update topup.js

--- plugins/topup.js
+++ plugins/topup.js
@@ -10,7 +10,7 @@ return await hikki.game.payDiamond(makeSession, '0895410636348')
 }
 const top = await topup() 
 conn.sendFile(m.chat, top.qrCode, 'Qris.jpg', 'Payment : ${top.paymentMethod}\nId : ${id}\nJumlah : ${jumlah} Diamond\nScan & Bayar Maksimal 30 detik setelah qr ini keluar', m)
-} catch (e) { return reply(`Sistem Error atau Nominal Diamond/Id\nUser Tidak Ada\nList Nominal Diamond\n5 Dm\n12 Dm\n70 Dm\n140 Dm\n355 Dm\n720 Dm`) }
+} catch (e) { return m.reply(`Sistem Error atau Nominal Diamond/Id\nUser Tidak Ada\nList Nominal Diamond\n5 Dm\n12 Dm\n70 Dm\n140 Dm\n355 Dm\n720 Dm`) }
 }
 handler.help = ['topupff id|jumlah']
 handler.tags = ['topup']
