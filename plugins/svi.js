fcd87e72243d0e04f9af6e54ba6307b7bc69f43b
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-06-20 00:56:23 +0700

    Update svi.js

--- plugins/svi.js
+++ plugins/svi.js
@@ -1,14 +1,14 @@
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async(m, { conn, text }) => {
-
-    if (!number) return conn.reply(m.chat, 'Silahkan masukan nomor yang akan di spam\n\n*Contoh:*\n.spam 6285xxxxxxxx', m)
+    text = args.join(' ')
+    if (!text) return conn.reply(m.chat, 'Silahkan masukan nomor yang akan di spam\n\n*Contoh:*\n.spam 6285xxxxxxxx', m)
     if (text.startsWith('+')) return conn.reply(m.chat, '[!] Tolong masukan Nomor dengan awalaan 62\n\n*Contoh:*\n.spam 6285xxxxxxxx|elyas ganteng', m)
     if (text.startsWith('@')) return conn.reply(m.chat, '[!] Tolong masukan Nomor dengan awalaan 62\n\n*Contoh:*\n.spam 6285xxxxxxxx|elyas ganteng', m)
 
-    let korban = `${number}`
+    let korban = `${text}`
     var nomor = m.sender
-    let spam1 = `*「 WA SPAMMER 」*\n\nPELAKU SPAM : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${pesan}\n\n\nBukan salah gw ya bang\nGw cuman BOT`
+    let spam1 = `*「 WA SPAMMER 」*\n\nPELAKU SPAM : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}\n\n\nBukan salah gw ya bang\nGw cuman BOT`
 
     conn.sendMessage(korban + '@s.whatsapp.net', {text:spam1}, {quoted:m})
     conn.sendMessage(korban + '@s.whatsapp.net', {text:spam1}, {quoted:m})
