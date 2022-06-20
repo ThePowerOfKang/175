3860424314d9571915251815d3daf10d39f4ad7a
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-06-20 11:42:04 +0700

    Update liputan6.js

--- plugins/liputan6.js
+++ plugins/liputan6.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch")
 let handler = async(m, { conn }) => {
    var a = await require('@bochilteam/scraper').liputan6()
    var b = JSON.parse(JSON.stringify(a))
-   var c = await conn.rand(b)
+   var c = await pickRandom(b)
    //var c = b[Math.floor(Math.random() * b.length)]
    var { title, link, image, description, date, label } = c
    var sell = `📺 *Liputan6 News*
@@ -19,3 +19,6 @@ handler.command = /^liputan6(news)?$/i
 handler.limit = true
 
 module.exports = handler
+function pickRandom(list) {
+  return list[Math.round(Math.random() * list.length)]
+}
