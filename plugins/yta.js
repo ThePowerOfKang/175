1562ff4a00e5371ba37e8b43e3e08fded59a903c
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-06-26 00:22:52 +0700

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -4,6 +4,7 @@ const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
   if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
   let chat = global.db.data.chats[m.chat]
+  let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   m.reply(wait)
